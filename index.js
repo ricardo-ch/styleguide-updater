fc3e31d088308aa59007056f18e5b32f4c1b638b
Kevin <kevin.herklotz@ricardo.ch>
2020-04-15 23:56:30 +0200

    jo

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 #!/usr/bin/env node
+const path = require("path");
+const escapeStringRegexp = require("escape-string-regexp");
+const replace = require("replace-in-file");
+const replaceList = require("./replaceList");
 
-console.log("--- it is working");
-console.log(`Current directory: ${process.cwd()}`);
+const asyncReplace = async () => {
+  let from = [];
+  let to = [];
+
+  replaceList.forEach((replacement) => {
+    from.push(new RegExp(escapeStringRegexp(replacement.replace), "g"));
+    to.push(replacement.with);
+  });
+
+  const directory = path.resolve(process.cwd(), process.argv[2]);
+  try {
+    const options = {
+      files: `${directory}/**`,
+      from,
+      to,
+      dry: true,
+      countMatches: true,
+    };
+
+    const results = await replace(options);
+    console.log(
+      "Changed files:",
+      results
+        .filter((result) => result.hasChanged)
+        .map((result) => {
+          return { file: result.file, count: result.numReplacements };
+        })
+    );
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+};
+
+asyncReplace();
