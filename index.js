dc79e652ffa1bfbf40e587dcc33a13b147a4ddfe
Kevin <kevin.herklotz@ricardo.ch>
2020-04-17 16:33:38 +0200

    jo

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-#!/usr/bin/env node
 const path = require("path");
 const escapeStringRegexp = require("escape-string-regexp");
 const replace = require("replace-in-file");
 const replaceList = require("./replaceList");
 
+if (!process.argv[2]) {
+  console.error("Error: You must provide a relative path as an argument!");
+  process.exit();
+}
+
+const convertToScssVariable = (JssVariable) =>
+  `$${JssVariable.replace("jssGlobals.", "").replace(/\./g, "_")}`;
+
 const asyncReplace = async () => {
   let from = [];
   let to = [];
 
+  // convert to "replace-in-file" compliant format for CSSinJS
   replaceList.forEach((replacement) => {
     from.push(new RegExp(escapeStringRegexp(replacement.replace), "g"));
     to.push(replacement.with);
+  });
+
+  // convert to "replace-in-file" compliant format for SCSS
+  replaceList.forEach((replacement) => {
+    const scssVariable = convertToScssVariable(replacement.replace);
+    from.push(new RegExp(escapeStringRegexp(scssVariable), "g"));
+
+    let scssValue;
+    if (
+      replacement.with[0] === "`" ||
+      replacement.with[0] === "'" ||
+      replacement.with[0] === '"'
+    ) {
+      scssValue = replacement.with;
+    } else {
+      scssValue = convertToScssVariable(replacement.with);
+    }
+    to.push(scssValue);
   });
 
   const directory = path.resolve(process.cwd(), process.argv[2]);
@@ -29,7 +55,7 @@ const asyncReplace = async () => {
       results
         .filter((result) => result.hasChanged)
         .map((result) => {
-          return { file: result.file, count: result.numReplacements };
+          return { file: result.file, changes: result.numReplacements };
         })
     );
   } catch (error) {
---
+++ readme.md
@@ -0,0 +1,7 @@
+# How to use
+
+```
+npx https://github.com/ricardo-ch/styleguide-updater src
+```
+
+That's it
